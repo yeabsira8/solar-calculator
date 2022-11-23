c573d82ccbec4f43a75f1abbdc5944d4d84f7f5e
YEABSIRA <101800269+yeabsira8@users.noreply.github.com>
2022-11-23 16:20:04 +0300

    Update scratch_14.js

--- js/scratch_14.js
+++ js/scratch_14.js
@@ -8,20 +8,25 @@
         var area = (area*0.75);
         var panels = Math.round((area)/1.64);
 
-        Number_of_panels_needed.innerHTML= Math.round((area)/1.64);
+        Number_of_panels_needed.innerHTML=Math.round((area)/1.64);
 
         capacity.innerHTML= Math.round(panels*295/1000)+ " kW";
 
         var bill = document.getElementById("bill").value;
-
         generation.innerHTML= Math.round(area*panel_efficiency*H*PR/1000)+ " MWh";
-        saving.innerHTML= Math.round(bill*12)+ " €";
+        var savings = Math.round(bill*12);
+        var parts = savings.toString().split(".");
+        var num = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+        saving.innerHTML=num + " €";
+
         var savings = (bill*12);
         var costs = ((panels*295)*3);
 
         payoff.innerHTML = Math.round(costs / savings) + " years";
-
-        e_cost.innerHTML = Math.round(costs) + " €";
+        var cost = Math.round(costs);
+        var parts = cost.toString().split(".");
+        var num1 = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+        e_cost.innerHTML = num1 + " €";
 
         roı.innerHTML = Math.round((((25*savings)-costs)/costs)*100)+ " % over lifetime";
 
@@ -29,11 +34,21 @@
 
         quote.innerHTML = quote1;
 
+        var x = document.getElementById("answers22");
+            x.style.display = "block";
 
+}
 
+function showResult1() {
+        var error = document.getElementById("error");
+        if (isNaN(document.getElementById("area").value))
+        {
 
-
-
-
-
+            // Changing HTML to draw attention
+            error.innerHTML = "<span style='color: red;'>"+
+                        "Please enter an area </span>"
+        } else {
+            error.innerHTML = ""
         }
+
+ }
